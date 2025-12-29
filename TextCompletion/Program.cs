d45d67fb87e91b120492bff34cfc62bf2b281fc2
leithgharbi <leithgharbi@yahoo.fr>
2025-12-29 16:21:19 +0100

    feat ( Text completion ) : add Classification, Sentiment Analysis and Summarization LLM examples

--- TextCompletion/Program.cs
+++ TextCompletion/Program.cs
@@ -2,8 +2,7 @@ using Microsoft.Extensions.AI;
 using Microsoft.Extensions.Configuration;
 using OpenAI;
 using System.ClientModel;
-using System;
-using System.Threading;
+
 
 // Build configuration with user secrets
 var configuration = new ConfigurationBuilder()
@@ -45,19 +44,55 @@ IChatClient chatClient = openAIClient.GetChatClient("openai/gpt-4o-mini").AsICha
 #endregion
 
 #region Streaming 
-var prompt = "What is generative AI? Explain in 200 words.";
-Console.WriteLine($"User Prompt >>>  {prompt} \n");
+//var prompt = "What is generative AI? Explain in 200 words.";
+//Console.WriteLine($"User Prompt >>>  {prompt} \n");
 
-var responseStream = chatClient.GetStreamingResponseAsync(prompt);
-await foreach (var chunk in responseStream)
-{
-    if (!string.IsNullOrEmpty(chunk.Text))
-    {
-        Console.Write(chunk.Text);
-    }
-}
+//var responseStream = chatClient.GetStreamingResponseAsync(prompt);
+//await foreach (var chunk in responseStream)
+//{
+//    if (!string.IsNullOrEmpty(chunk.Text))
+//    {
+//        Console.Write(chunk.Text);
+//    }
+//}
 #endregion
 
+#region Classification
 
+//var classificationPrompt = """
+//Please classify the following sentences into categories: 
+//- 'complaint' 
+//- 'suggestion' 
+//- 'praise' 
+//- 'other'.
 
+//1) "I love the new layout!"
+//2) "You should add a night mode."
+//3) "When I try to log in, it keeps failing."
+//4) "This app is decent."
+//""";
+
+//Console.WriteLine($"user >>> {classificationPrompt}");
+
+//ChatResponse classificationResponse = await client.GetResponseAsync(classificationPrompt);
+
+//Console.WriteLine($"assistant >>>\n{classificationResponse}");
+
+#endregion
+
+#region Summarization
+
+//var summaryPrompt = """
+//Summarize the following blog in 1 concise sentences:
+
+//"Microservices architecture is increasingly popular for building complex applications, but it comes with additional overhead. It's crucial to ensure each service is as small and focused as possible, and that the team invests in robust CI/CD pipelines to manage deployments and updates. Proper monitoring is also essential to maintain reliability as the system grows."
+//""";
+
+//Console.WriteLine($"user >>> {summaryPrompt}");
+
+//ChatResponse summaryResponse = await client.GetResponseAsync(summaryPrompt);
+
+//Console.WriteLine($"assistant >>> \n{summaryResponse}");
+
+#endregion
 
