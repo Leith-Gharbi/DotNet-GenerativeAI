94a565ed20d0566cde0522b839ae0934415b8c6e
leithgharbi <leithgharbi@yahoo.fr>
2025-12-29 19:43:23 +0100

    feat ( Text completion ) : add new console app for text completion with ollama local LLM

---
+++ TextCompletion.AI.Ollama/Program.cs
@@ -0,0 +1,9 @@
+﻿
+
+using Microsoft.Extensions.AI;
+using OllamaSharp;
+
+IChatClient client = 
+    new OllamaApiClient(new Uri("http://127.0.0.1:1234") , "Qwen2.5-7B-Instruct-1M-Q4_K_M.gguf");
+
+
